e6c23caa6a0f39bc24940910890a577fd99b6fca
LIESard <liesfromalizard@gmail.com>
2014-12-05 01:59:02 +0100

    Updated Main.java

--- Main.java
+++ Main.java
@@ -6,38 +6,39 @@ import org.jsfml.window.event.Event;
 
 public class Main {
 
-	public static final int WIDTH = 640, HEIGHT = 480;	// This sets the window's size
-	public static final String TITLE = "JSFML Window";	// Sets the window's height
+	public static final int WIDTH = 640, HEIGHT = 480;
+	public static final String TITLE = "JSFML Window";	// Set 3 variables, the window's width, height and title
 
 	private VideoMode mode;					// I don't really know how to explain what this is, so just add it.  In a way, it's the window's parameters
 	private RenderWindow window;				// A render target (something for you to put sprites on).  A window, in this case
 
-	public Main() {						// Constructor
-		createObjects();				// Calls method
+	public Main() {
+		createObjects();
 
-		while (window.isOpen()) {			// Calls methods as long as the window didn't close
+		while (window.isOpen()) {
 			draw();
 			input();
 		}
 	}
 
 	public void createObjects() {
-		mode	= new VideoMode(WIDTH, HEIGHT);		// Initialises the VideoMode to the height and width we want
-		window	= new RenderWindow();			// Initialises the RenderWindow
+		mode	= new VideoMode(WIDTH, HEIGHT);
+		window	= new RenderWindow();
 
 		window.create(mode, TITLE);			// Draws the RenderWindow with the three parameters we set at the beginning
-								// You can also add another param, which is Window.FULLSCREEN.  I'm sure you can guess what that does.
+								// You can also add another param, which is Window.FULLSCREEN.
 	}
 
 	public void draw() {
 		window.clear();					// This clears the window so that you don't get duplicate sprites whenever something moves
-		window.display();				// This displays the window.
+		// Put window.draw(drawable object) to draw stuff.  Read up on sprites, textures and images to find out more
+		window.display();
 	}
 
 	public void input() {
 		for (Event event : window.pollEvents()) {	// If you want a key event for moving sprites, put it outside this for loop
-			if (event.type == Event.Type.CLOSED || Keyboard.isKeyPressed(Key.ESCAPE)) {	// Checks to see if the escape button or cross button was pressed
-				window.close();			// Closes the RenderWindow
+			if (event.type == Event.Type.CLOSED || Keyboard.isKeyPressed(Key.ESCAPE)) {
+				window.close();			// Closes the window when close is requested
 				System.exit(0);			// Exits the program with a clean exit
 			}
 		}
